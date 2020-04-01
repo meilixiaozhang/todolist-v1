b77c39c5da8e08de4a7f7bda485417d3313af227
meilixiaozhang <redpangci@gmail.com>
2020-04-01 21:44:32 +1100

    add header abd footer ejs

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname+"/date.js");
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -14,27 +15,23 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  let today = new Date();
-  let currentDay = today.getDay();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
-
-
+  const day = date.getDate();
   res.render("list", {
     listTitle: day,
     newListItems: items
   })
 });
 
+app.get("/work", function(req, res) {
+  res.render("list", {listTitle: "Work", newListItems: workItems});
+})
+
+app.get("/about", function(req, res) {
+  res.render("about");
+})
+
 app.post("/", function(req, res) {
-  console.log(req.body)
-  let item = req.body.newItem;
+  const item = req.body.newItem;
 
   if (req.body.list == "Work") {
     workItems.push(item);
@@ -44,10 +41,6 @@ app.post("/", function(req, res) {
     res.redirect("/");
   }
 });
-
-app.get("/work", function(req, res) {
-  res.render("list", {listTitle: "Work", newListItems: workItems});
-})
 
 app.listen(3000, function() {
   console.log("Server started on server 3000.");
