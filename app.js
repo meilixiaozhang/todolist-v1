21515ff395dab2e3f5968920eb6d40d230fc9791
meilixiaozhang <redpangci@gmail.com>
2020-04-01 16:23:05 +1100

    add work route

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -26,7 +27,7 @@ app.get("/", function(req, res) {
 
 
   res.render("list", {
-    kindOfDay: day,
+    listTitle: day,
     newListItems: items
   })
 });
@@ -36,6 +37,16 @@ app.post("/", function(req, res) {
   items.push(item);
   res.redirect("/");
 });
+
+app.get("/work", function(req, res) {
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+})
+
+app.post("/work", function(req, res) {
+  let item = req.body.newItems;
+  items.push(item);
+  res.redirect("/work");
+})
 
 
 app.listen(3000, function() {
